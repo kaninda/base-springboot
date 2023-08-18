297ccd5983674ceb68f5735cf713b1fe1822ba72
kaninda <73389238+kaninda@users.noreply.github.com>
2023-08-18 07:33:38 +0200

    Updating company service

---
+++ src/test/java/com/aka/base/BaseSpringBootTests.java
@@ -0,0 +1,12 @@
+package com.aka.base;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class BaseSpringBootTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
