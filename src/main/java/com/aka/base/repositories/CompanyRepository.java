177ae979ae01022c3b16f0242330aa8c3a040553
kaninda <73389238+kaninda@users.noreply.github.com>
2023-08-18 07:25:30 +0200

    Updating company service

--- src/main/java/com/aka/base/repositories/CompanyRepository.java
+++ src/main/java/com/aka/base/repositories/CompanyRepository.java
@@ -2,7 +2,10 @@ package com.aka.base.repositories;
 
 import com.aka.base.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompanyRepository extends JpaRepository <Company, Long> {
     Company findByName(String name);
+    @Query("select case when count(c) > 0 then true else false end from Company c where c.name= ?1")
+    Boolean selectExistsName (String name);
 }
